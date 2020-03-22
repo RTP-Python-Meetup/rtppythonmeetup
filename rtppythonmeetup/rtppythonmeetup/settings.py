9d4919673972e9d0a88e1bf8cc6b7fa33a2ba305
Malcolm VanOrder <mvanorder1390@gmail.com>
2020-03-22 23:54:50 +0000

    Add domain name to allowed hosts
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- rtppythonmeetup/rtppythonmeetup/settings.py
+++ rtppythonmeetup/rtppythonmeetup/settings.py
@@ -29,7 +29,7 @@ except FileNotFoundError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'rtppythonmeetup.com', 'www.rtppythonmeetup.com']
 
 
 # Application definition
